5f1fef8653f2d2d3e129671c6b7635cf5e45d176
SeoSang <ddrrpg@naver.com>
2020-10-23 18:23:05 +0900

    인덱스 컴퍼넌트분리

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,13 +9,10 @@ import {
 } from '@material-ui/core';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import IndexSlide from '../components/IndexSlide';
+import IndexMain from '../components/IndexMain';
 import SearchBar from '../components/SearchBar';
 import SubjectTable from '../components/SubjectTable';
 import { subjects } from '../dummy';
-import { useTypedSelector } from '../features';
-import { addTest, resetTest, userSlice } from '../features/user/userSclice';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -32,26 +29,10 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-let intervalId = 0 as any;
 export default function Home() {
   const classes = useStyles();
-  const { test } = useTypedSelector(state => state.user);
   const [submitClicked, setSubmitClicked] = useState(false);
   const [searchKeyword, setSearchKeyword] = useState('');
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
-
-  const intervalStart = () => {
-    if (intervalId) return;
-    intervalId = setInterval(() => {
-      dispatch(addTest());
-    }, 1000);
-  };
 
   return (
     <div className={classes.root}>
@@ -61,42 +42,7 @@ export default function Home() {
         onSubmit={() => setSubmitClicked(true)}
       />
       {!submitClicked ? (
-        <div className={classes.root}>
-          <div className={classes.slide}>
-            <IndexSlide subjects={subjects}></IndexSlide>
-          </div>
-          <Typography variant="h4">{test}</Typography>
-          <div>
-            <Button variant="contained" onClick={intervalStart}>
-              카운팅 시작
-            </Button>
-            <Button
-              variant="contained"
-              onClick={() => {
-                clearInterval(intervalId);
-                intervalId = 0;
-              }}
-            >
-              카운팅 멈추기
-            </Button>
-            <Button
-              variant="contained"
-              onClick={() => {
-                dispatch(addTest());
-              }}
-            >
-              그냥 올리기
-            </Button>
-            <Button
-              variant="contained"
-              onClick={() => {
-                dispatch(resetTest());
-              }}
-            >
-              리셋
-            </Button>
-          </div>
-        </div>
+        <IndexMain></IndexMain>
       ) : (
         <SubjectTable subjects={subjects} />
       )}
