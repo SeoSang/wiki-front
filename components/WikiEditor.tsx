f8a169095cdbb20dd57de5bb497d6173aedbfa0a
hangooksaram <hyunjae1995@gmail.com>
2020-11-07 22:36:02 +0900

    :feat 위키 에디터 UI 추가중

--- components/WikiEditor.tsx
+++ components/WikiEditor.tsx
@@ -1,45 +1,90 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/styles'
-import {Paper} from '@material-ui/core'
+import {makeStyles} from '@material-ui/styles';
+import {Paper, Button,Divider} from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-const useStyles = makeStyles(() => ({
+const indexlist = [{
+    id : '1',
+    content : '큰 목차 1'
+},{
+    id : '1.1',
+    content : '작은 목차 1'
+},
+ {
+    id : '2',
+    content: '큰 목차 2'
+},
+]
+
+const useStyles = makeStyles((props) => ({
     root : {
-        width : '100%',
+        minWidth : '100vh',
         minHeight : '100vh',
         display :'flex',
-        flexDirection : 'column',
-        alignItems : 'center',
+        flexDirection : 'column',  
+        paddingLeft : '2%',
+    },
+    title : {
+        fontSize : '50px',
     },
     indexContainer : {
-        width : '100%',
-        display :'flex',
-        flexDirection : 'column',
-        alignItems : 'start',
-        paddingLeft : '10%'
-    },
-    index : {
-        width : '33%',
+        width : '33vh',
         height : '100%',
         display :'flex',
+        paddingLeft : '1%',
         flexDirection : 'column',
-        alignItems : 'center'
-    }
+        alignItems : 'start',
+        paddingBottom : '1%'
+    },
+    indexList : {
+        
+    },
+    indexTitle : {
+        width : '100%',
+        fontSize : '18px',
+        marginBottom : '4px',
+    },
+    indexSubTitle : {
+        width : '100%',
+        paddingLeft : '15px',
+        fontSize : '16px',
+        marginBottom : '4px',
+    },
+    contentsContainer : {
+        width : '100%',
+        display : 'flex',
+        flexDirection : 'column',
+        alignItems : 'start',
+        marginTop : '3%',
+    },
+    
 }))
 
 export default function WikiEditor(){
     const classes = useStyles();
     return(
         <Paper className = {classes.root}>
-            <h1>Wiki Editor</h1>
-            <div className = {classes.indexContainer}>
-                <Paper className ={classes.index} variant="outlined" >
-                    목차
-                    <br></br>
-                    <br></br>
-                    <br></br><br></br><br></br><br></br><br></br>
-                    ddd
+            <h1 className = {classes.title}>신화와 철학</h1>
+            {/* 목차 */}
+            <div>
+                <Paper className ={classes.indexContainer} variant="outlined" >
+                    <h2>목차</h2>
+                    <div className={classes.indexList}>
+                    {indexlist.map(i => <div className = {i.id.length > 1 ? classes.indexSubTitle : classes.indexTitle} >
+                        <span>{i.id}. {i.content}</span>
+                    </div>
+                    )}
+                    </div>
                 </Paper>
-                </div>
+            </div>
+
+            {/* 컨텐츠 */}
+            <div className = {classes.contentsContainer}>
+                {indexlist.map(i =><div style={{fontSize:'30px'}} className = {classes.indexTitle} >
+                        <span><Button><ExpandMoreIcon/></Button>{i.id}. {i.content} </span>
+                        <Divider/>
+                    </div>)}
+            </div>
         </Paper>
 
     )
--- pages/trash.tsx
+++ pages/trash.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import WikiEditor from '../components/WikiEditor'
+import WikiEditor from './../components/WikiEditor';
 
 export default function trash () {
-    return(
+    return (
         <WikiEditor/>
     )
 }
