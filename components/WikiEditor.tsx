e5a7969c3b13d03ba1a43c09a10002c424ae758c
hangooksaram <hyunjae1995@gmail.com>
2020-11-09 13:41:43 +0900

    :feat 위키 에디터 UI 만드는 중

--- components/WikiEditor.tsx
+++ components/WikiEditor.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {makeStyles} from '@material-ui/styles';
 import {Paper, Button,Divider} from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const indexlist = [{
     id : '1',
-    content : '큰 목차 1'
+    title : '큰 목차 1',
+    content : '이것은 큰 목차1'
 },{
     id : '1.1',
-    content : '작은 목차 1'
+    title : '작은 목차 1',
+    content : '이것은 작은 목차1'
 },
  {
     id : '2',
-    content: '큰 목차 2'
+    title: '큰 목차 2',
+    content : '이것은 큰 목차2'
 },
 ]
 
@@ -59,9 +62,14 @@ const useStyles = makeStyles((props) => ({
     },
     
 }))
-
 export default function WikiEditor(){
     const classes = useStyles();
+    const [isContentOpened, setIsContentOpened] = useState("");
+    const openContent = (id) => {
+        //setIsContentOpened(isContentOpened => isContentOpened.concat(id));
+        setIsContentOpened(id);
+    }
+
     return(
         <Paper className = {classes.root}>
             <h1 className = {classes.title}>신화와 철학</h1>
@@ -71,7 +79,7 @@ export default function WikiEditor(){
                     <h2>목차</h2>
                     <div className={classes.indexList}>
                     {indexlist.map(i => <div className = {i.id.length > 1 ? classes.indexSubTitle : classes.indexTitle} >
-                        <span>{i.id}. {i.content}</span>
+                        <span>{i.id}. {i.title}</span>
                     </div>
                     )}
                     </div>
@@ -81,8 +89,9 @@ export default function WikiEditor(){
             {/* 컨텐츠 */}
             <div className = {classes.contentsContainer}>
                 {indexlist.map(i =><div style={{fontSize:'30px'}} className = {classes.indexTitle} >
-                        <span><Button><ExpandMoreIcon/></Button>{i.id}. {i.content} </span>
-                        <Divider/>
+                        <span><Button onClick={() => openContent(i.id)}><ExpandMoreIcon/></Button>{i.id}. {i.title} </span>                        
+                        <Divider/>                        
+                        {isContentOpened === i.id ? <div>{i.content}</div> : ""}
                     </div>)}
             </div>
         </Paper>
