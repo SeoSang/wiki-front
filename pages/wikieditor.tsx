a825e629a6f9960c653bd074d1e7cc43c423edf2
hangooksaram <hyunjae1995@gmail.com>
2020-11-25 22:07:55 +0900

    feat: 위키 에디터 만드는 중

--- pages/wikieditor.tsx
+++ pages/wikieditor.tsx
@@ -1,16 +1,18 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import {makeStyles} from '@material-ui/styles';
 import {Paper, Button,Divider} from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import WikiContents from './wikicontents';
+import { editorContainerStyles } from '../styles/cssStyle'
+import 'react-quill/dist/quill.snow.css';
 
-interface indexlist {
+interface IndexList {
     id : string,
     title : string,
     content : string
 }
 
-const indexlist:indexlist[] = [{
+const indexlist:IndexList[] = [{
     id : '1',
     title : '큰 목차 1',
     content : '이것은 큰 목차1'
@@ -24,82 +26,77 @@ const indexlist:indexlist[] = [{
     title: '큰 목차 2',
     content : '이것은 큰 목차2'
 },
+{
+   id : '3',
+   title: '큰 목차 3',
+   content : '이것은 큰 목차3'
+},
+{
+   id : '4',
+   title: '큰 목차 4',
+   content : '이것은 큰 목차4'
+},
 ]
 
-const useStyles = makeStyles((props) => ({
-    root : {
-        minWidth : '100vh',
-        minHeight : '100vh',
-        display :'flex',
-        flexDirection : 'column',  
-        paddingLeft : '2%',
-    },
-    title : {
-        fontSize : '50px',
-    },
-    indexContainer : {
-        width : '33vh',
-        height : '100%',
-        display :'flex',
-        paddingLeft : '1%',
-        flexDirection : 'column',
-        alignItems : 'start',
-        paddingBottom : '1%'
-    },
-    indexList : {
-        
-    },
-    indexTitle : {
-        width : '100%',
-        fontSize : '18px',
-        marginBottom : '4px',
-    },
-    indexSubTitle : {
-        width : '100%',
-        paddingLeft : '15px',
-        fontSize : '16px',
-        marginBottom : '4px',
-    },
-    contentsContainer : {
-        width : '100%',
-        display : 'flex',
-        flexDirection : 'column',
-        alignItems : 'start',
-        marginTop : '3%',
-    },
-    
-}))
 export default function WikiEditor(){
-    const classes = useStyles();
-    const [isContentOpened, setIsContentOpened] = useState("");
-    const openContent = (id) => {
-        //setIsContentOpened(isContentOpened => isContentOpened.concat(id));
-        setIsContentOpened(id);
-    }
+    const contentsRef = useRef({});
+    const classes = editorContainerStyles();
+    const [isContentOpened, setIsContentOpened] = useState([]);
 
+     const onClickIndex = (id) => {  
+         let focusContent = indexlist.find(i => i.id === id);
+         contentsRef.current = focusContent;
+         contentsRef.current.focus();
+     }
+    const openContent = (id) => {        
+        if(isContentOpened.find(c=> c === id)){
+            setIsContentOpened(isContentOpened => isContentOpened.filter(c=> c !== id));
+        }
+        else setIsContentOpened(isContentOpened => isContentOpened.concat(id));        
+    } 
+    const openEditor = () => {
+        setIsEditorOpened(true);        
+    }
+    const ReactQuill =
+    typeof window === 'object' ? require('react-quill') : () => false;
+    const [value, setValue] = useState('');
+    const [isEditorOpened, setIsEditorOpened] = useState(false);
     return(
         <Paper className = {classes.root}>
-            <h1 className = {classes.title}>신화와 철학</h1>
-            {/* 목차 */}
-            <div>
-                <Paper className ={classes.indexContainer} variant="outlined" >
-                    <h2>목차</h2>
-                    <div className={classes.indexList}>
-                    {indexlist.map(i => <div className = {i.id.length > 1 ? classes.indexSubTitle : classes.indexTitle} >
-                        <span>{i.id}. {i.title}</span>
-                    </div>
-                    )}
-                    </div>
-                </Paper>
+            
+            <div className = {classes.titleContainer}>
+                <h1 className = {classes.title}>신화와 철학</h1>
+                <Button variant="contained" color ="primary" className ={classes.titleButton}>게시판 이동</Button>
             </div>
+            
+            <Paper className ={classes.indexContainer} variant="outlined" >
+                <h2>목차</h2>                    
+                    {indexlist.map(i => 
+                        <div className = {i.id.length > 1 ? classes.indexSubTitle : classes.indexTitle}>
+                            <Button onClick={() => onClickIndex}>{i.id}. {i.title}</Button>
+                        </div>)}
+            </Paper>
 
-            {/* 컨텐츠 */}
             <div className = {classes.contentsContainer}>
-                {indexlist.map(i =><div style={{fontSize:'30px'}} className = {classes.indexTitle} >
-                        <span><Button onClick={() => openContent(i.id)}><ExpandMoreIcon/></Button>{i.id}. {i.title} </span>                        
-                        <Divider/>                        
-                        {/* {isContentOpened === i.id ? <WikiContents content={i.content}/> : ""}                       */}
-                        {isContentOpened === i.id ? i.content :""}
+                {indexlist.map(i =>
+                    <div style={{fontSize:'30px'}} className = {classes.indexTitle}>               
+                        <div className={classes.contentsRow}>
+                            <span><Button onClick={() => openContent(i.id)}><ExpandMoreIcon/></Button>{i.id}. {i.title} </span>                                                
+                            <span style={{textDecoration : 'underline'}}>
+                                <Button onClick={()=> openEditor()}>수정</Button>
+                            </span>
+                        </div>
+                        <Divider/>                     
+                        {isContentOpened.find(m => m === i.id) ? i.content :""}
+                        {
+                            isEditorOpened ?
+                        <ReactQuill
+                            style={{ textAlign: 'left' }}
+                            theme="snow"
+                            value={value}
+                            onChange={setValue}
+                        /> : ""
+                        }
                     </div>)}
             </div>
         </Paper>
