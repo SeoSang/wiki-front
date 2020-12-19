cf7881832a2b295642e16d2c26cb8b48d446e6a5
SeoSang <ddrrpg@naver.com>
2020-12-19 22:20:36 +0900

    메인에 공지사항 완료, 관리자 페이지 틀 만듦

---
+++ pages/admin.tsx
@@ -0,0 +1,121 @@
+import React from 'react';
+import SwipeableViews from 'react-swipeable-views';
+import { makeStyles, Theme, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { useMarginStyles } from '../styles/cssStyle';
+import { VolumeUp } from '@material-ui/icons';
+import AccessibilityIcon from '@material-ui/icons/Accessibility';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  dir?: string;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: any) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  admin: {
+    backgroundColor: theme.palette.background.paper,
+    width: '80%',
+  },
+  root: {
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    alignContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+}));
+
+export default function FullWidthTabs() {
+  const classes = useStyles();
+  const mar = useMarginStyles();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index: number) => {
+    setValue(index);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Typography className={mar.marBottom2} variant="h4">
+        {' '}
+        관리자 페이지
+      </Typography>
+      <div className={classes.admin}>
+        <AppBar position="static" color="default">
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="fullWidth"
+            aria-label="full width tabs example"
+          >
+            <Tab label="공지사항" icon={<VolumeUp />} {...a11yProps(0)} />
+            <Tab
+              label="유저조회"
+              icon={<AccessibilityIcon />}
+              {...a11yProps(1)}
+            />
+            <Tab label="신고현황" icon={<AssignmentIcon />} {...a11yProps(2)} />
+          </Tabs>
+        </AppBar>
+        <SwipeableViews
+          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+          index={value}
+          onChangeIndex={handleChangeIndex}
+        >
+          <TabPanel value={value} index={0} dir={theme.direction}>
+            Item One
+          </TabPanel>
+          <TabPanel value={value} index={1} dir={theme.direction}>
+            Item Two
+          </TabPanel>
+          <TabPanel value={value} index={2} dir={theme.direction}>
+            Item Three
+          </TabPanel>
+        </SwipeableViews>
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,21 +6,22 @@ import {
   IconButton,
   InputBase,
   makeStyles,
+  TextField,
   Theme,
   Typography,
 } from '@material-ui/core';
 import SurroundSoundIcon from '@material-ui/icons/SurroundSound';
-import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import IndexMain from '../components/IndexMain';
 import SearchBar from '../components/SearchBar';
 import SubjectTable from '../components/SubjectTable';
-import { subjects } from '../dummy';
-import { loadPosts } from '../features/board/action';
 import { searchSubjects } from '../features/subject/action';
 import { useDivStyles, useMarginStyles } from '../styles/cssStyle';
 import clsx from 'clsx';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import ArrowDropDownCircleIcon from '@material-ui/icons/ArrowDropDownCircle';
+import VolumeUpIcon from '@material-ui/icons/VolumeUp';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -37,17 +38,31 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     notice: {
       width: '100%',
+      border: '1px dashed gray',
+      padding: theme.spacing(0.5),
+    },
+    iconContainer: {
+      height: '100%',
     },
     noticeContainer: {
       height: '100%',
       width: '100%',
     },
     firstLineContainer: {
-      border: '1px solid orange',
+      padding: theme.spacing(0.5),
+      border: '3px solid #ffa62b',
       borderRadius: '5px',
     },
   })
 );
+
+const dummyNotice = [
+  '공지1 입니다~~~~~',
+  '공지2 입니다~~~~~~~',
+  '공지3!',
+  '공지4',
+  '공지5',
+];
 
 export default function Home() {
   const classes = useStyles();
@@ -56,6 +71,7 @@ export default function Home() {
   const dispatch = useDispatch();
   const mar = useMarginStyles();
   const div = useDivStyles();
+  const [noticeIndex, setNoticeIndex] = useState(0);
 
   const onClickSearch = () => {
     dispatch(searchSubjects({ searchName }));
@@ -65,23 +81,52 @@ export default function Home() {
   return (
     <div className={classes.root}>
       <Grid
-        className={clsx(mar.marBottom2, classes.firstLineContainer)}
         container
+        className={clsx(mar.marBottom2, classes.firstLineContainer)}
+        direction="row"
+        justify="center"
       >
-        <Grid item md={1} sm={2}>
-          <IconButton>
-            <SurroundSoundIcon
+        <Grid item xs={2} md={1}>
+          <div className={div.centerFlex}>
+            <IconButton>
+              <SurroundSoundIcon
+                color="secondary"
+                fontSize="large"
+              ></SurroundSoundIcon>
+            </IconButton>
+          </div>
+        </Grid>
+        <Grid item xs={8} md={10}>
+          <div className={clsx(classes.noticeContainer, div.centerColFlex)}>
+            <div className={classes.notice}>
+              <Grid container>
+                <Grid item xs={2} md={1}>
+                  <div
+                    className={clsx(classes.iconContainer, div.centerColFlex)}
+                  >
+                    <VolumeUpIcon></VolumeUpIcon>
+                  </div>
+                </Grid>
+                <Grid item xs={10} md={11}>
+                  <Typography variant="h6" align="center">
+                    {dummyNotice[noticeIndex]}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </div>
+          </div>
+        </Grid>
+        <Grid item xs={2} md={1}>
+          <IconButton
+            onClick={() => {
+              setNoticeIndex((noticeIndex + 1) % dummyNotice.length);
+            }}
+          >
+            <ArrowDropDownCircleIcon
               color="secondary"
               fontSize="large"
-            ></SurroundSoundIcon>
+            ></ArrowDropDownCircleIcon>
           </IconButton>
-        </Grid>
-        <Grid item md={10} sm={9}>
-          <div className={clsx(div.centerFlex, classes.noticeContainer)}>
-            <Button className={classes.notice} variant="outlined">
-              공지게시글1
-            </Button>
-          </div>
         </Grid>
       </Grid>
       <SearchBar
