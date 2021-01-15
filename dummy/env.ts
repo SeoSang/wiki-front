52e2a7918c49feca0e60e996a1f7027f151fd125
SeoSang <ddrrpg@naver.com>
2021-01-15 20:07:07 +0900

    환경변수 테스트

--- dummy/env.ts
+++ dummy/env.ts
@@ -1,5 +1,1 @@
-export const PRODUCTION_BACKEND_URL = process.env.production;
-
-export const BACKEND_URL = PRODUCTION_BACKEND_URL
-  ? PRODUCTION_BACKEND_URL
-  : 'http://localhost:8080/project_wings_war';
+export const BACKEND_URL = process.env.URL;
