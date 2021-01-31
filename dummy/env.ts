da4779a7cd4efa0923c6aced3b6b7812c0d98728
SeoSang <ddrrpg@naver.com>
2021-01-31 22:37:58 +0900

    Change Backend Server

--- dummy/env.ts
+++ dummy/env.ts
@@ -1,5 +1,3 @@
 export const PRODUCTION_BACKEND_URL = process.env.production;
 
-export const BACKEND_URL = PRODUCTION_BACKEND_URL
-  ? PRODUCTION_BACKEND_URL
-  : 'http://localhost:8080/mywiki';
+export const BACKEND_URL = 'https://5e4112277eb0.ngrok.io/mywiki';
